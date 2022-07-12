100d05c7c4932c54c9c9acd39ae5ae33f65ec52d
SoJaeBeom <sojb0826@gmail.com>
2022-07-13 08:39:17 +0900

    [Feat] 메인 페이지 추가

--- app.py
+++ app.py
@@ -1,1 +1,54 @@
-# app.py 입니다.
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('main.html')
+
+@app.route('/addcard')
+def addcard():
+   return render_template('addcard.html')
+
+@app.route("/movie-list", methods=["POST"])
+def movie_save():
+    movie_list = list(db.movies.find({}, {'_id': False}))
+
+    if len(movie_list) == 0:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        data = requests.get('https://movie.naver.com/movie/running/current.naver', headers=headers)
+
+        soup = BeautifulSoup(data.text, 'html.parser')
+
+        ul = soup.find('ul', class_='lst_detail_t1')
+        img = ul.select('li > div > a > img')
+
+        count = 0
+
+        for i in img:
+            count = count + 1
+            movie = {
+                'num': count,
+                'movie_name': i.get('alt'),
+                'movie_img': i.get('src')
+            }
+            db.movies.insert_one(movie)
+
+    movie_list = list(db.movies.find({}, {'_id': False}))
+    return jsonify({'movies': movie_list})
+
+@app.route("/movie-list", methods=["GET"])
+def movie_get():
+    movie = db.movies.find_one({'num': int(request.args.get('id'))}, {'_id': False})
+    return jsonify({'movies': movie})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
--- static/main.js
+++ static/main.js
@@ -1,1 +1,39 @@
-console.log('main 페이지 입니다!')
+$(document).ready(function () {
+    listing();
+});
+
+function listing() {
+    $.ajax({
+        type: 'POST',
+        url: '/movie-list',
+        data: {},
+        success: function (response) {
+            let rows = response['movies']
+            console.log(rows)
+            for (let i = 0; i < rows.length; i++) {
+                let name = rows[i]['movie_name'];
+                let img = rows[i]['movie_img'];
+                let num = rows[i]['num']
+                let temp_html = `
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="${img}" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">${name}</h5>
+                        </div>
+                        <div>
+                            <button onclick="clickBtn(${num})" class="default_button">만들기</button>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#movie_list').append(temp_html)
+            }
+        }
+    });
+}
+
+function clickBtn(num) {
+    console.log(num)
+    window.location.href = "/addcard?id=" + num
+}
