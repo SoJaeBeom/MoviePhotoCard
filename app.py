a7d45af6012acfb868af998247a305d24aac7ef8
SoJaeBeom <sojb0826@gmail.com>
2022-07-13 15:16:48 +0900

    [Feat] 카드 생성 페이지 추가

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ def addcard():
 @app.route("/movie-list", methods=["POST"])
 def movie_save():
     movie_list = list(db.movies.find({}, {'_id': False}))
-
     if len(movie_list) == 0:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -49,6 +48,25 @@ def movie_save():
 def movie_get():
     movie = db.movies.find_one({'num': int(request.args.get('id'))}, {'_id': False})
     return jsonify({'movies': movie})
+
+@app.route("/add-card", methods=["POST"])
+def card_post():
+    movie_name_receive = request.form['movie_name_give']
+    movie_img_receive = request.form['movie_img_give']
+    date_receive = request.form['date_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    card = {
+        'movie_name': movie_name_receive,
+        'movie_img': movie_img_receive,
+        'date': date_receive,
+        'star': star_receive,
+        'comment': comment_receive
+    }
+
+    db.cards.insert_one(card)
+    return jsonify({'result': 'ok'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
